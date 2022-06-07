afc6df53f718b734ef1f3948693cf82cc2669b33
RafaelSoaresDev <rafael.ol.soaress@gmail.com>
2022-06-07 18:36:28 -0300

    Implementação do eventListener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 var currentNumberWrapper = document.getElementById('currentNumber');
+var adicionar = document.getElementsByClassName('adicionar');
+var subtrair = document.getElementsByClassName('subtrair')
 var currentNumber = 0;
 
 function increment() {
@@ -10,3 +12,6 @@ function decrement() {
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
 }
+
+adicionar[0].addEventListener('click', increment, false);
+subtrair[0].addEventListener('click', decrement, false);
